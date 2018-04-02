0573d3aa6cee24beff724cc1c58c52beb867eb97
Jonas Karlsson <Jonas.k94@hotmail.com>
2018-04-02 22:04:02 +0200

    Fixed Alarm bugs Removed floating action button

---
+++ app/src/main/java/zelongames/travelarm_clock/DialogHelper.java
@@ -0,0 +1,27 @@
+package zelongames.travelarm_clock;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class DialogHelper {
+
+    public static AlertDialog.Builder createDialogBuilder(Context context, String title, String message){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setMessage(message).setTitle(title);
+
+        return alertDialogBuilder;
+    }
+
+    public static Dialog createPositiveNegativeDialog(
+            Context context, String title, String message, String positiveBtnText, String negativeBtnText,
+            DialogInterface.OnClickListener positiveOnClick, DialogInterface.OnClickListener negativeOnClick){
+        AlertDialog.Builder builder = createDialogBuilder(context, title, message);
+
+        builder.setPositiveButton(positiveBtnText, positiveOnClick);
+        builder.setNegativeButton(negativeBtnText, negativeOnClick);
+
+        return builder.create();
+    }
+}
